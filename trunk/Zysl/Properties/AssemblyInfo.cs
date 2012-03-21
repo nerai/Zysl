826b84ea4f4742c515773e10267a33c382d8560e
Sebastian Heuchler <mafu@mafutrct.de>
2012-03-21 13:13:54 +0100

    fix name

--- trunk/Zysl/Properties/AssemblyInfo.cs
+++ trunk/Zysl/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle ("Ezysl")]
+[assembly: AssemblyTitle ("Zysl")]
 [assembly: AssemblyDescription ("")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
-[assembly: AssemblyProduct ("Ezysl")]
+[assembly: AssemblyProduct ("Zysl")]
 [assembly: AssemblyCopyright ("Copyright © 2012 Sebastian 'mafutrct' Heuchler")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
